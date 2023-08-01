3529934a043714d99610b8c4cead9ceb243356e9
aagraston <aagraston@gmail.com>
2023-08-01 09:55:29 -0700

    Starting gatsby blog project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
-import * as React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>I am Aaron Graston, an aspiring developer living in beautiful Kelowna, BC</h2>
+    </div>
+  )
 }
+
+export default IndexPage
