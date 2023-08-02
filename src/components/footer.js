2c460d2512a0b797dcffeffbcead5fcf216e29fe
aagraston <aagraston@gmail.com>
2023-08-02 09:40:09 -0700

    styling for the blog

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {graphql, useStaticQuery} from 'gatsby';
+import * as footerStyles from './footer.module.scss'
 
 const Footer = () => {
   const data = useStaticQuery(graphql`
@@ -12,7 +13,7 @@ const Footer = () => {
   }
   `)
   return (
-    <footer>
+    <footer className={footerStyles.footer}>
       <p>created by {data.site.siteMetadata.author}, © <em>{data.site.siteMetadata.author}, 2023</em></p>
     </footer>
   );
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout";
 import { Link, graphql, useStaticQuery } from "gatsby";
+import * as blogStyles from "./blog.module.scss";
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -24,12 +25,12 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map((edge) => {
-          const slug = edge.node.fields.slug
-          const linkString = `/blog/${slug}`
-          return(
-            <li>
+          const slug = edge.node.fields.slug;
+          const linkString = `/blog/${slug}`;
+          return (
+            <li className={blogStyles.post}>
               <h2>{edge.node.frontmatter.title}</h2>
               <p>{edge.node.frontmatter.date}</p>
               <p>
